fea30fa9d74fda1cf6a37d5840df632e132f4ef4
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-09-13 18:51:52 +0200

    'ok'

--- config/secret.js
+++ config/secret.js
@@ -6,13 +6,15 @@ module.exports = {
         clientID: process.env.FACEBOOK_ID || '805870756251090',
         clientSecret: process.env.FACEBOOK_SECRET || '2fa76b134375cebe21fcbef8268bdd5b',
         profileFields: ['emails', 'displayName'],
-        callbackURL: 'http://localhost:3000/auth/facebook/callback/'
+        callbackURL: 'https://agile-dawn-93506.herokuapp.com/auth/facebook/callback/'
+
+        // callbackURL: 'http://localhost:3000/auth/facebook/callback/'
     },
     twitter: {
         consumerKey: process.env.TWITTER_ID || 'YU9Rioq6BZkS7G7OtspToeOGE',
         consumerSecret: process.env.TWITTER_SECRET || 'ht254juKPXzUtteXFDI849DZQ1GUQwmyeiRmptKT9eC3uV6wnk',
         profileFields: ['emails', 'displayName'],
-        callbackURL: 'http://localhost:3000/auth/twitter/callback/'
+        callbackURL: 'https://agile-dawn-93506.herokuapp.com/auth/twitter/callback/'
     },
     companyName: 'أتميدة أون لاين ستور',
     projectName: 'أتميدة أون لاين ستور',
