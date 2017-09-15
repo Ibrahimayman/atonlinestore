9f7ac1d3d9040638b288f97846b19f157ca5b583
ibrahimayman <eng.ibrahim.cer@hotmail.com>
2017-09-15 02:09:52 +0200

    'ok'

--- public/myApp/profile/controller.js
+++ public/myApp/profile/controller.js
@@ -1,8 +1,24 @@
 app.controller("saveProfileCtrl", function ($scope, $http, toaster) {
 
-    $scope.formData = {};
-    setBirthDateFunc();
+    var userDays = [];
+    for (var i = 1; i < 32; i++) {
+        userDays.push(i);
+    }
+    $scope.days = userDays;
 
+    var userMonths = [];
+    for (var i = 1; i < 13; i++) {
+        userMonths.push(i);
+    }
+    $scope.Months = userMonths;
+
+    var userYears = [];
+    for (var i = 1975; i <2010 ; i++) {
+        userYears.push(i);
+    }
+    $scope.years = userYears;
+
+    setBirthDateFunc();
     function setBirthDateFunc() {
         $http.get('/api/profile/setBirthDate/')
             .success(function (data) {
@@ -11,13 +27,10 @@ app.controller("saveProfileCtrl", function ($scope, $http, toaster) {
                 var month = date.getMonth();
                 var year = date.getFullYear();
                 var day = date.getDate();
-
-                $scope.day = day;
-
+                // set values for birth date.
                 $("[name='day']").val(day);
                 $("[name='month']").val(month);
                 $("[name='year']").val(year);
-
             })
             .error(function (data) {
                 console.log('Error: ' + data);
